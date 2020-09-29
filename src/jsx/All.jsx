1e10e7594fc2cf80d150ecc163ded0c941ef3cfb
waouo <f5864565@gmail.com>
2020-09-29 16:03:18 +0800

    countdown

--- src/jsx/All.jsx
+++ src/jsx/All.jsx
@@ -1,81 +1,190 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+class Length extends React.PureComponent  {
+    render() {
+        const type = this.props.type
+
+        return (
+            <div id={`${type}-length-control`} className='m-4 d-inline-block'>
+                <h2 id={`${type}-label`}>{type} Length</h2>
+                <h3 className='text-center'>
+                    <button id={`${type}-increment`} onClick={this.props.onLengthChange}><i className="fa fa-arrow-up" /></button>
+                    <span id={`${type}-length`}>{this.props.length}</span>
+                    <button id={`${type}-decrement`} onClick={this.props.onLengthChange}><i className="fa fa-arrow-down" /></button>
+                </h3>
+            </div>
+        )
+    }
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            breakLength: '1',
-            sessionLength: '25:00',
+            breakLength: 1,
+            sessionLength: 25,
+            timer: 1500,
+            clockType: 'session',
+            clockStatus: 'stopped',
+            intervalID: '',
         }
-        this.onSessionChange = this.onSessionChange.bind(this)
+        this.onLengthChange = this.onLengthChange.bind(this)
+        this.onTimerControl = this.onTimerControl.bind(this)
+        this.onReset = this.onReset.bind(this)
+        this.clockify = this.clockify.bind(this)
     }
 
-    onSessionChange(e) {
-        const btn = e.target.value
-        const { breakLength, sessionLength } = this.state
-        let breakLengthResult, sessionLengthResult
+    onLengthChange(e) {
+        const btn = e.currentTarget.id
+        const { breakLength, sessionLength, clockStatus } = this.state
+        let breakLengthRes = breakLength
+        let sessionLengthRes = sessionLength
 
-        console.log('a');
+        if (clockStatus === 'running') return;
+
         switch (btn) {
-            case 'break-up':
+            case 'break-increment':
+                if (breakLength < 60) breakLengthRes = breakLength + 1
+                else break
                 break
-            case 'break-down':
+            case 'break-decrement':
+                if (breakLength > 1) breakLengthRes = breakLength - 1
+                else break
                 break
-            case 'session-up':
+            case 'session-increment':
+                if (sessionLength < 60) sessionLengthRes = sessionLength + 1
+                else break
                 break
-            case 'session-down':
+            case 'session-decrement':
+                if (sessionLength > 1) sessionLengthRes = sessionLength - 1
+                else break
                 break
             default:
                 break
         }
 
         this.setState({
-            breakLength: breakLengthResult,
-            sessionLength: sessionLengthResult,
+            breakLength: breakLengthRes,
+            sessionLength: sessionLengthRes,
+            timer: sessionLengthRes * 60,
         })
     }
 
+    onTimerControl() {
+        const { clockStatus, intervalID } = this.state
+
+        if (clockStatus === 'stopped') {
+            this.setState({ clockStatus: 'running' })
+            this.beingCountDown('running')
+        } else if (clockStatus === 'running') {
+            clearInterval(intervalID)
+            this.setState({
+                clockStatus: 'stopped',
+                intervalID: '',
+            })
+        }
+    }
+
+    beingCountDown() {
+        this.setState(() => ({
+            intervalID: setInterval(() => {
+                this.timerDec()
+                this.phaseControl()
+            }, 1000)
+        }))
+    }
+
+    timerDec() {
+        this.setState(state => ({ timer: state.timer - 1 }))
+    }
+
+    phaseControl() {
+        const { timer, clockType, breakLength, sessionLength } = this.state
+
+        if (timer <= 0) {
+            this.buzzer()
+            if (clockType === 'session') {
+                this.setState({
+                    clockType: 'break',
+                    timer: breakLength * 60
+                })
+            } else {
+                this.setState({
+                    clockType: 'session',
+                    timer: sessionLength * 60
+                })
+            }
+        }
+    }
+
+    buzzer() {
+        const audio = document.querySelector('audio')
+        audio.play()
+    }
+
+    clockify() {
+        const { timer } = this.state
+        let minutes = Math.floor(timer / 60)
+        let seconds = timer - minutes * 60
+
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        minutes = minutes < 10 ? '0' + minutes : minutes
+
+        return minutes + ':' + seconds
+    }
+
+
+    onReset() {
+        const sessionLength = this.state.sessionLength
+
+        clearInterval(this.state.intervalID)
+
+        this.setState({
+            timer: sessionLength * 60,
+            clockType: 'session',
+            clockStatus: 'stopped',
+            intervalID: '',
+        })
+    }
+
+
     render() {
+        let timeStyle
+        if (this.state.timer <= 60) {
+            timeStyle = { color: 'red' }
+        } else {
+            timeStyle = { color: 'white' }
+        }
 
         return (
             <div id='app'>
-                <h1 className='text-center'>25 + 5 Clock</h1>
-                <div id="break-length-control" className='m-4 d-inline-block'>
-                    <h2 id='break-label'>Break Length</h2>
-                    <h3 className='text-center'>
-                        <button id='break-increment' onClick={this.onSessionChange}><i className="fa fa-arrow-up" /></button>
-                        <span id='break-length'>{this.state.breakLength}</span>
-                        <button id='break-decrement' onClick={this.onSessionChange}><i class="fa fa-arrow-down" /></button>
-                    </h3>
-                </div>
-                <div id="session-length-control" className='m-4 d-inline-block'>
-                    <h2 id="session-label">Session Length</h2>
-                    <h3 className='text-center'>
-                        <button id='session-increment' onClick={this.onSessionChange}><i className="fa fa-arrow-up" /></button>
-                        <span id='break-length'>{this.state.breakLength}</span>
-                        <button id='session-decrement' onClick={this.onSessionChange}><i class="fa fa-arrow-down" /></button>
-                    </h3>
-                </div>
+                <h1 className='text-center' >25 + 5 Clock</h1>
+                <Length type={'break'} length={this.state.breakLength} onLengthChange={this.onLengthChange} />
+                <Length type={'session'} length={this.state.sessionLength} onLengthChange={this.onLengthChange} />
+
                 <div id="clock" className='col mx-auto'>
-                    <h2 id='timer-label' className='text-center my-1'>Session</h2>
-                    <h2 id='time-left' className='text-center'>{this.state.sessionLength}</h2>
+                    <h2 id='timer-label' className='text-center my-1'>{this.state.clockType}</h2>
+                    <h2 id='time-left' className='text-center' style={timeStyle}>{this.clockify()}</h2>
                 </div>
                 <div className='d-flex justify-content-center'>
-                    <div id="start_stop">
-                        <button><i id='start' class="fas fa-play clock-control" /></button>
-                        <button><i id='stop' class="fas fa-stop clock-control" /></button>
-                    </div>
-                    <button><i id='reset' class="fas fa-redo clock-control" /></button>
+                    <button onClick={this.onTimerControl}>
+                        <i id='start' className="fas fa-play clock-control pr-0 mr-0" />
+                        <i id='stop' className="fas fa-stop clock-control pl-0 ml-0" />
+                    </button>
+                    <button onClick={this.onReset}><i id='reset' className="fas fa-redo clock-control" /></button>
                 </div>
-                <hr/>
+                <hr />
                 <footer>
-                    <p className='text-center m-0' style={{color: 'red'}}>Designed and Coded by</p>
-                    <p className='text-center m-0' style={{color: 'blue'}}>Shao Wei</p>
+                    <p className='text-center m-0' style={{ color: 'red' }}>Designed and Coded by</p>
+                    <p className='text-center m-0' style={{ color: 'blue' }}>Shao Wei</p>
                 </footer>
+                <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
             </div>
         )
     }
 }
+
+
 
 ReactDom.render(<App />, document.getElementById('react-root'))
