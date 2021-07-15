7d8c97a4c46fb4541863244070941b8c746161e6
Waouo <66577859+Waouo@users.noreply.github.com>
2021-07-15 19:40:15 +0800

    上傳打包檔案

--- webpack.config.js
+++ webpack.config.js
@@ -6,71 +6,76 @@ const {
 } = require('clean-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, './src'),
-    entry: {
-        index: ['./main.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'App'),
-        filename: 'index-bundle.js'
-    },
-    module: {
-        rules: [{
-                test: /\.s[ac]ss$/i,
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                }, "css-loader", "sass-loader"],
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    index: ['./main.js'],
+  },
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
             },
-            {
-                test: /\.js$/i,
-                exclude: [/node_modules/],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/i,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.jsx$/i,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
             },
-            {
-                test: /\.jsx$/i,
-                exclude: [/node_modules/],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/'
-                        }
-                    }
-                ]
-            }
-
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'src/[name].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            filename: 'index.html'
-        }),
-        new CleanWebpackPlugin(),
+          },
+        ],
+      },
     ],
-    devServer: {
-        port: 8000,
-        open: false,
-    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'src/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 8000,
+    open: false,
+  },
 }
